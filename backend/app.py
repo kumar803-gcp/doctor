83a5dfc2c04e9b6c19bf27a8eabe324a503eefb2
kumar <kumarswamy@Rraj95155outlookcom>
2025-08-16 17:32:02 +0530

    extra features added

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-
 appointments = []
 
 @app.route('/', methods=['GET', 'POST'])
@@ -9,9 +8,18 @@ def index():
     if request.method == 'POST':
         name = request.form['name']
         doctor = request.form['doctor']
-        appointments.append({'name': name, 'doctor': doctor})
-        return redirect(url_for('index'))
-    return render_template('index.html', appointments=appointments)
+        issue = request.form['issue']
+        days = request.form['days']
+        symptoms = request.form['symptoms']
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        appointments.append({
+            'name': name,
+            'doctor': doctor,
+            'issue': issue,
+            'days': days,
+            'symptoms': symptoms
+        })
+
+        return redirect(url_for('index'))
+
+    return render_template('index.html', appointments=appointments)
